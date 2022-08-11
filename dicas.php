86cd4e129ec558247885738a6300e48ce46e0427
Leandro Dukievicz <ldukie@icloud.com>
2022-08-10 23:26:21 -0300

    update page 'dicas'

--- dicas.php
+++ dicas.php
@@ -21,8 +21,9 @@
 ?>
 
 <Section >
-<h2>Confira aqui algumas Receitas que Dona Dirce Indica</h2>
-    <div id="dicas">
+    <h2>Confira aqui algumas Receitas que Dona Dirce Indica</h2>
+    
+    <div class="dicas">
         <div>
             
             <figure id="img-head">
@@ -34,7 +35,8 @@
                 </a>
                 
                 <a href="https://www.tudogostoso.com.br/receita/136814-escondidinho-de-carne-moida.html" target="_blank">
-                    <img class="img-dicas click" src="img/capa/2.jpg" alt=""> <br><br><br>
+                    <img class="img-dicas click" src="img/capa/2.jpg" alt="">
+                                                                                 <br><br><br>
                     <button class=" btn-dicas img-dicas click">  <strong>Clique para Ver a receita</strong></button>
                 </a>
 
@@ -48,26 +50,86 @@
            
         </div>
        
+
+
     </div>
+
+    <?php
+        include( 'contato.php');
+    ?>
+    
+    <h5> Esta Seção do Site ainda está Recebendo atualições ! Assine Nossa Newsletter para ficar sempre atualizado !</h5>
+    <form action="" method="post"> 
+
+            <p style="color: #FFF"><b>Está chegando muitas novidades no cardápio da Dona Dirce,
+            Informe o seu melhor E-mail que te avisaremos !</b>
+            </p>&nbsp;&nbsp;
+            <input type="text"  placeholder="Insira o seu nome ">&nbsp;
+            <input type="email" placeholder="insira seu melhor Email !">&nbsp;
+            <button  id="btn-form"class="click" type="submit"><strong>Enviar</strong> </button>
+
+    </form>
+
+</Section>
+
+<Section >
+    <h2>Confira aqui algumas Receitas que Dona Dirce Indica</h2>
+    
+    <div class="dicas">
+        <div>
+            
+            <figure id="img-head">
+           
+                <a href="https://www.tudogostoso.com.br/receita/173520-crepioca-pao-de-queijo-de-frigideira.html" target="_blank">
+                
+                    <img class="img-dicas click" src="img/capa/1.jpg" alt="" ><br><br><br>
+                    <button class="btn-dicas img-dicas click"> <strong>Clique para Ver a receita</strong></button>
+                </a>
+                
+                <a href="https://www.tudogostoso.com.br/receita/136814-escondidinho-de-carne-moida.html" target="_blank">
+                    <img class="img-dicas click" src="img/capa/2.jpg" alt="">
+                                                                                 <br><br><br>
+                    <button class=" btn-dicas img-dicas click">  <strong>Clique para Ver a receita</strong></button>
+                </a>
+
+                <a href="https://www.tudogostoso.com.br/receita/125788-salpicao-de-frango-simples.html" target="_blank">
+                    <img class="img-dicas click"src="img/capa/3.jpg" alt=""><br><br><br>
+                    <button  class="btn-dicas img-dicas click"> <strong>Clique para Ver a receita</strong> </button>
+                </a>
+
+
+            </figure>
+           
+        </div>
+       
+
+
+    </div>
+
+    <?php
+        include( 'contato.php');
+    ?>
+    
+    <h5> Esta Seção do Site ainda está Recebendo atualições ! Assine Nossa Newsletter para ficar sempre atualizado !</h5>
+    <form action="" method="post"> 
+
+            <p style="color: #FFF"><b>Está chegando muitas novidades no cardápio da Dona Dirce,
+            Informe o seu melhor E-mail que te avisaremos !</b>
+            </p>&nbsp;&nbsp;
+            <input type="text"  placeholder="Insira o seu nome ">&nbsp;
+            <input type="email" placeholder="insira seu melhor Email !">&nbsp;
+            <button  id="btn-form"class="click" type="submit"><strong>Enviar</strong> </button>
+
+    </form>
+
 </Section>
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 <?php
-    include('contato.php');
+    
     include ( 'rodape.php');
 
 ?>
