d55b20c4fd818824794a7034f675ef9b030f9096
Leandro Dukievicz <ldukie@icloud.com>
2022-08-11 12:10:04 -0300

    update project and add page 'videos'

--- dicas.php
+++ dicas.php
@@ -10,128 +10,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="assets/cutlery.PNG" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/dicas.css">
+    <link rel="stylesheet" href="css/dicasevideos.css">
     <title>Dicas de Receitas</title>
 </head>
-
-
 
 <?php
     include ('cabecalho.php');
 ?>
 
 <Section >
-    <h2>Confira aqui algumas Receitas que Dona Dirce Indica</h2>
-    
+    <h2>Confira aqui algumas Receitas que Dona Dirce Indica</h2>    
     <div class="dicas">
         <div>
-            
             <figure id="img-head">
-           
                 <a href="https://www.tudogostoso.com.br/receita/173520-crepioca-pao-de-queijo-de-frigideira.html" target="_blank">
-                
-                    <img class="img-dicas click" src="img/capa/1.jpg" alt="" ><br><br><br>
-                    <button class="btn-dicas img-dicas click"> <strong>Clique para Ver a receita</strong></button>
+                    <img class="img-dicas click" src="img/capa/1.jpg" 
+                    
+                    alt="foto de uma receita do site - 
+                    crepioca de pao de queijo de frigideira, imagem também é um link ao clicar vai ser redirecionado
+                    para a página do site contendo a receita " ><br><br><br>
+                    
+                    <button class="btn-dicas img-dicas click"> 
+                    <strong>Clique para Ver a receita</strong></button>
                 </a>
                 
                 <a href="https://www.tudogostoso.com.br/receita/136814-escondidinho-de-carne-moida.html" target="_blank">
-                    <img class="img-dicas click" src="img/capa/2.jpg" alt="">
-                                                                                 <br><br><br>
-                    <button class=" btn-dicas img-dicas click">  <strong>Clique para Ver a receita</strong></button>
+                    <img class="img-dicas click" src="img/capa/2.jpg" 
+                    
+                    alt="Foto de Receita - Escondidinho de carne moída - a foto é um link e ao clicar em cima
+                    vai ser redirecionado para a página contendo a receita e o vídeo">
+
+                    <br><br><br>
+                    <button class=" btn-dicas img-dicas click">  
+                    <strong>Clique para Ver a receita</strong>
+                    </button>
                 </a>
 
                 <a href="https://www.tudogostoso.com.br/receita/125788-salpicao-de-frango-simples.html" target="_blank">
-                    <img class="img-dicas click"src="img/capa/3.jpg" alt=""><br><br><br>
-                    <button  class="btn-dicas img-dicas click"> <strong>Clique para Ver a receita</strong> </button>
+                    <img class="img-dicas click"src="img/capa/3.jpg" 
+                    
+                    alt="Foto da receita de Salpicão de frango simples - imagem é um link 
+                    e ao clicar será redirecionado para a página contendo a receita ">
+                    
+                    <br><br><br>
+                    <button  class="btn-dicas img-dicas click">
+                    <strong>Clique para Ver a receita</strong>
+                    </button>
                 </a>
-
-
             </figure>
-           
         </div>
        
-
-
     </div>
 
     <?php
         include( 'contato.php');
     ?>
     
-    <h5> Esta Seção do Site ainda está Recebendo atualições ! Assine Nossa Newsletter para ficar sempre atualizado !</h5>
-    <form action="" method="post"> 
+        <h5> Esta Seção do Site ainda está Recebendo atualições ! Assine Nossa Newsletter para ficar sempre atualizado! <br>
+            Fonte de Receitas desta Página 
+            <a href="https://www.tudogostoso.com.br/">Tudo Gostoso</a>
+        </h5>
 
+        <form action="" method="post"> 
             <p style="color: #FFF"><b>Está chegando muitas novidades no cardápio da Dona Dirce,
-            Informe o seu melhor E-mail que te avisaremos !</b>
+                                      Informe o seu melhor E-mail que te avisaremos !</b>
             </p>&nbsp;&nbsp;
             <input type="text"  placeholder="Insira o seu nome ">&nbsp;
-            <input type="email" placeholder="insira seu melhor Email !">&nbsp;
+            <input type="email" placeholder="Insira seu melhor Email !">&nbsp;
             <button  id="btn-form"class="click" type="submit"><strong>Enviar</strong> </button>
-
-    </form>
-
+        </form>
 </Section>
-
-<Section >
-    <h2>Confira aqui algumas Receitas que Dona Dirce Indica</h2>
-    
-    <div class="dicas">
-        <div>
-            
-            <figure id="img-head">
-           
-                <a href="https://www.tudogostoso.com.br/receita/173520-crepioca-pao-de-queijo-de-frigideira.html" target="_blank">
-                
-                    <img class="img-dicas click" src="img/capa/1.jpg" alt="" ><br><br><br>
-                    <button class="btn-dicas img-dicas click"> <strong>Clique para Ver a receita</strong></button>
-                </a>
-                
-                <a href="https://www.tudogostoso.com.br/receita/136814-escondidinho-de-carne-moida.html" target="_blank">
-                    <img class="img-dicas click" src="img/capa/2.jpg" alt="">
-                                                                                 <br><br><br>
-                    <button class=" btn-dicas img-dicas click">  <strong>Clique para Ver a receita</strong></button>
-                </a>
-
-                <a href="https://www.tudogostoso.com.br/receita/125788-salpicao-de-frango-simples.html" target="_blank">
-                    <img class="img-dicas click"src="img/capa/3.jpg" alt=""><br><br><br>
-                    <button  class="btn-dicas img-dicas click"> <strong>Clique para Ver a receita</strong> </button>
-                </a>
-
-
-            </figure>
-           
-        </div>
-       
-
-
-    </div>
-
-    <?php
-        include( 'contato.php');
-    ?>
-    
-    <h5> Esta Seção do Site ainda está Recebendo atualições ! Assine Nossa Newsletter para ficar sempre atualizado !</h5>
-    <form action="" method="post"> 
-
-            <p style="color: #FFF"><b>Está chegando muitas novidades no cardápio da Dona Dirce,
-            Informe o seu melhor E-mail que te avisaremos !</b>
-            </p>&nbsp;&nbsp;
-            <input type="text"  placeholder="Insira o seu nome ">&nbsp;
-            <input type="email" placeholder="insira seu melhor Email !">&nbsp;
-            <button  id="btn-form"class="click" type="submit"><strong>Enviar</strong> </button>
-
-    </form>
-
-</Section>
-
-
-
-
 
 <?php
-    
     include ( 'rodape.php');
-
 ?>
 
 
--- videos.php
+++ videos.php
@@ -10,29 +10,79 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="assets/cutlery.PNG" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/dicas.css">
     <title>Vídeos de Receitas</title>
 </head>
-
-
 
 <?php
     include ('cabecalho.php');
 ?>
 
+<Section >
+    <h2>Confira aqui as novidades no canal da Dirce</h2>    
+    <div class="dicas">
+        <div>
+            <figure class="mp4">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/OJJtdm-OQ4M" 
+                    title="YouTube video player" frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; 
+                    encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
+                
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/E8RhIjnQRO4" 
+                    title="YouTube video player" frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+                    picture-in-picture" allowfullscreen>
+                </iframe>       
+            </figure>
+        </div>
+    </div>
+</Section>
 
-
-
-
-
+<Section >
+     
+    <div class="dicas">
+        <div>
+            <figure class="mp4">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/ykdve13-Vao" 
+                    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;
+                     clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>
+                
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/-PQfWtmuhGg" 
+                    title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+                    clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                </iframe>  
+            </figure>
+            <h2>Em Breve Teremos mais novidades por aqui!</h2>   
+        </div>
+    </div>
+</Section>
 
 <?php
-        include('contato.php');
+        include( 'contato.php');
     ?>
 
+<h5> Esta Seção do Site ainda está Recebendo atualições ! Assine Nossa Newsletter para ficar sempre atualizado! <br>
+            Fonte de Receitas desta Página 
+            <a href="https://www.tudogostoso.com.br/">Tudo Gostoso</a>
+        </h5>
+
+        <form action="" method="post"> 
+            <p style="color: #FFF"><b>Está chegando muitas novidades no cardápio da Dona Dirce,
+                                      Informe o seu melhor E-mail que te avisaremos !</b>
+            </p>&nbsp;&nbsp;
+            <input type="text"  placeholder="Insira o seu nome ">&nbsp;
+            <input type="email" placeholder="Insira seu melhor Email !">&nbsp;
+            <button  id="btn-form"class="click" type="submit"><strong>Enviar</strong> </button>
+        </form>
 
 
 <?php
     include ( 'rodape.php');
-
 ?>
+
+
+
+
 
