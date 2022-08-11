f39acf6238b4f4f4b6862b2c3d450731dd359108
Leandro Dukievicz <ldukie@icloud.com>
2022-08-11 20:44:12 -0300

    update code

--- cardapio.php
+++ cardapio.php
@@ -30,7 +30,35 @@ $cardapio_caseiro = array(
      'foto'=>'img/caseira/caseira-op-06.jpg'),
 );
 
+$cardapio_saudavel = array(
+    0=> array('id'=>'1','titulo'=>'Opção 01','descricao'=>'Frango grelhado, mandioca, brócolis e cenoura salteados na manteiga e salada.', 'preco'=>'Pequena R$ 14.00, Média R$ 16.00, Grande R$ 18.00',
+    'foto'=>'img/saudavel/saudavel-op-01.jpg'),
 
+    1=> array('id'=>'2','titulo'=>'Opção 02','descricao'=>'Bife Grelhado, acompanhamento de vagem salteada
+    na manteiga, cenoura, purê de batata.', 'preco'=>'Pequena R$15.00 Média R$ 17.00 Grande R$ 19.50','
+    foto'=>'img/saudavel/saudavel-op-02.jpg'),
+
+    2=> array('id'=>'3','titulo'=>'Opção 03','descricao'=>'Filé de Tilápia grelhado, creme de Abóbora Cabotiá,
+    Abóbora Paulista refogada, salada de couve-flor, salada de tomate e alface.','preco'=>'Pequena R$ 16.00
+    Média R$ 18.00 Grande R$ 21.00', 'foto'=>'img/saudavel/saudavel-op-03.jpg'),
+
+    3=> array('id'=>'4','titulo'=>'Opção 04', 'descricao'=>'Salada fria de peito de Frango desfiado, tomates cereja, alface americana, cenoura ralada, vagem e grão de bico.', 'preco'=>'Pequena R$ 13.00 Média R$ 15.00 Grande R$  17.50', 'foto'=>'img/saudavel/saudavel-op-04.jpg'),
+);
+
+$cardapio_especial = array(
+    0=> array('id'=>'1','titulo'=>'Feijoada', 'descricao'=>'Feijoada caseira da dona Dirce, Acompanha 
+    Couve Refogada, arroz, Torresmo e vinagrete','preco'=>'Pequena R$ 20.00 Média R$ 25.00 Grande R$ 30.00',
+    'foto'=>'img/especiais/feijoada.jpg'),
+
+    1=> array('id'=>'2','titulo'=>'Lasanha Bolonhesa','descricao'=>'Lasanha bolonhesa de Carne Moída, 
+    acompanhamento porção de arroz. Serve 02 Pessoas ','preco'=>'R$ 37.50',
+    'foto'=>'img/especiais/lasanha.jpg'),
+
+    2=> array('id'=>'3','titulo'=>'Frango Assado','descricao'=>'Frango Assado todos os finais de semana e feriados','preco'=>'R$ 20.00','foto'=>'img/especiais/frango.jpg'),
+
+    3=> array('id'=>'4', 'titulo'=>'Costela Assada','descricao'=>'Costela Assada todos os finais 
+    de semana e feriados','preco'=>'R$ 37.00 KG','foto'=>'img/especiais/costela.jpg'),
+);
 
 
 
